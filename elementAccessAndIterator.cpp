de1c81731e0bdc858ba930f5e6e4e253c07ae813
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-07-09 12:15:11 +0600

    Explored list element access and iterators

---
+++ elementAccessAndIterator.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    list<int> firstList = {10, 20, 30, 40, 50};
+    // cout << firstList.back() << endl;
+    // cout << firstList.front() << endl;
+    // cout << *next(firstList.begin(), 2) << endl;
+    cout << *firstList.begin() << endl;
+    // cout << *firstList.end() << endl;
+
+    return 0;
+}
