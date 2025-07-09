0754ccbfe264efffdb380073f5098e85fcbc8547
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-07-09 11:06:06 +0600

    Still exploring list modifiers

--- listModifiers.cpp
+++ listModifiers.cpp
@@ -4,6 +4,22 @@ using namespace std;
 int main()
 {
     list<int> firstList = {10, 20, 30};
+    list<int> secondList = {100, 200};
+    vector<int> firstVector = {2, 3, 4};
+    // // secondList = firstList;
+    // secondList.assign(firstList.begin(), firstList.end());
+
+    // firstList.push_back(40);
+    // firstList.push_front(100);
+    // firstList.pop_back();
+
+    // cout << *next(firstList.begin(), 2) << endl;
+    firstList.insert(next(firstList.begin(), 2), firstVector.begin(), firstVector.end());
+    // firstList.pop_front();
+    for (int value : firstList)
+    {
+        cout << value << " ";
+    }
 
     return 0;
 }
